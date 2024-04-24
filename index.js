00ba54a0d45c3d9f2647f47ee74c3ff8c58dc059
Serikbol <sh.serikbol@gmail.com>
2024-04-24 17:42:01 +0500

    init homework 4

--- index.js
+++ index.js
@@ -1,65 +1,35 @@
 // Task 1
-// Practice communicating with the user via alert / confirm / prompt
-// let user = {};
-
-// user.name = prompt("Как вас зовут?");
-// user.age = prompt("Сколько вам лет?");
-// user.gender = confirm("Вы мужчина? Выберите 'Ок' если да, 'Отмена' если нет");
-
-// if (user.gender === true) {
-//     user.gender = "Мужчина"
-// } else {
-//     user.gender = "Женщина"
+// function checkAge (age) {
+//    return age > 18 ? true : confirm ("Родители разрешили?")
 // }
 
-// console.log("Имя пользователя:", user.name);
-// console.log("Возраст пользователя:", user.age);
-// console.log("Пол пользователя:", user.gender);
+function checkAge (age) {
+    return age > 18 || confirm ("Родители разрешили?")
+ }
+ console.log(checkAge(56));
 
-// alert("name: " + user.name + "\nage: " + user.age + "\ngender: " + user.gender);
 
-// Task 2
-// Working with operators
-// let number = prompt();
+//  Task 2
+function pow(x, n) {
+    let result = 1;
 
-// if (!isNaN(number)) {
-//     alert(number) 
-// }  else {
-//     alert(NaN)
-// }
-// console.log(number); // Надеюсь, что понял условие задачи правильно
-
-// Task 3
-// Working with conditions
-// let a = +prompt('a?', '');
-// console.log(String(a)) // string
-// a = +a
-// console.log(Number(a)) // number
-
-// switch(a) {
-//     case 0:
-//         alert( 0 );
-//         break;  
-//     case 1: 
-//         alert( 1 );
-//         break;
-//     case 2:
-//     case 3:
-//         alert( '2,3' );
-//     default: 
-//         undefined;
-// }
-
-//Task 4
-// Working with loops
-let n = 2;
-let sum = 0;
-while (n<=100) {
-    if(n%2 === 0) {
-        // alert( `number ${n}!` );
-        sum = sum + n; // or sum += n (это я для себя, все время забываю, что они идентичны)
+    for (let i = 0; i < n; i++) {
+        result = result * x;
     }
-    n++;
-}
-console.log(sum);
 
+    return result;
+}
+
+console.log(pow(3, 3), pow(3, 2), pow(1, 100))
+
+// Task 3 
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no();
+  }
+
+  ask(
+    "Вы согласны?",
+    () => { alert("Вы согласились."); },
+    () => { alert("Вы отменили выполнение."); }
+  );
