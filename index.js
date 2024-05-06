384c025f9ef3f8d0d7dc8899f1d8d6b5811d841f
Serikbol <sh.serikbol@gmail.com>
2024-05-06 15:28:36 +0500

    finished homework-4

--- index.js
+++ index.js
@@ -41,12 +41,52 @@ arr.sort((a, b) => a - b);
 // alert(arr); // 8, 5, 2, 1, -10
 
 // ================================== Task 5 ===================================
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map(user => user.name);
+
+// alert( names ); // Вася, Петя, Маша
+
+// ================================== Task 6 ===================================
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
 let users = [ vasya, petya, masha ];
 
-let names = users.map(user => user.name);
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+  }));
 
-// alert( names ); // Вася, Петя, Маша
+  /*usersMapped = [
+        { fullName: "Вася Пупкин", id: 1 },
+        { fullName: "Петя Иванов", id: 2 },
+        { fullName: "Маша Петрова", id: 3 }
+    ]
+    */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+// ================================== Task 7 ===================================
+let roma = { name: "Рома", age: 40 };
+let natya = { name: "Настя", age: 33 };
+let gena = { name: "Гена", age: 62 };
+
+let middleAge = [ roma, natya, gena ];
+
+function getAverageAge(users) {
+    if (users.length === 0) {
+        return 0;
+    }
+    let sum = users.reduce((total, user) => total + user.age, 0);
+    return sum / users.length;
+}
+
+
+alert( getAverageAge(middleAge) ); // (40 + 33 + 62) / 3 = 45
