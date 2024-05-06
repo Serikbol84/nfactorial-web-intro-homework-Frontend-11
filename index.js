ec3eef3bd85d937f7a2faf46c6065c3a1989221d
Serikbol <sh.serikbol@gmail.com>
2024-05-06 22:45:21 +0500

    5th home work done

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 // ============================== Task 1 ===================================
 let ageTable = document.getElementById("age-table");
-console.log(ageTable)
+console.log("Таблица с id=age-table: ", ageTable);
 
 let allLables = document.querySelectorAll("#age-table label");
-console.log(allLables);
+console.log("Все элементы label: ",  allLables);
 
-let firstTd = document.querySelector("#age-table td:first-child");
-console.log(firstTd);
+let firstTdInTable = document.querySelector("#age-table td:first-child");
+console.log("Первый td: ", firstTdInTable);
 
 let searchForm = document.querySelector('form[name="search"]');
-console.log(searchForm);
+console.log("Форма form с именем name=search: ", searchForm);
 
-let firstInput= document.querySelector('form[name="search"] input:first-of-type');
-console.log(firstInput);
+let inputs = searchForm.querySelectorAll("input"); // создаем переменную inputs, чтобы найти все input-ы в форме searchForm
 
-let lastInput= document.querySelector('form[name="search"] input:last-of-type');
-console.log(lastInput);
+let firstInputInSearchForm = inputs[0];
+console.log("Первый Input в форме: ", firstInputInSearchForm);
+
+let lastInputInSearchForm = inputs[inputs.length - 1];
+console.log("Последний Input в форме: ", lastInputInSearchForm); 
+//на последних двух чуть голову не сломал. Первый input более-менее лекго нашел
+//там у меня было так: let firstInputInSearchForm = document.querySelector('form[name="search"] input:first-of-type'); 
+//и даже просто ('form[name="search"] input') работал (кажется), а ('form[name="search"] input:last-of-type') так не работал. Почему?
 
 // ============================== Task 2 ===================================
+let body = document.baby;
+
+body.innerHTML = "<!--" + body.tagName + "-->";
+
+alert( body.firstChild.data ); // что выведет?
+//alert выдает (или выводит) текст BODY 
+//(не совсем понял эту тему если честно. Вижу только, что alert выдает этот текст)
