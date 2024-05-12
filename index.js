575943b29839d0cef57f71733d40906745efb8bb
Serikbol <sh.serikbol@gmail.com>
2024-05-12 14:16:37 +0500

    1-st and 2-nd tasks done

---
+++ index.js
@@ -0,0 +1,20 @@
+//=================================== Task 1 ===========================================
+// let button = document.getElementById ("hider"); 
+
+// button.addEventListener("click", function () {
+//     let hideText = document.getElementById("text");
+//     hideText.style.display = "none";
+// });
+
+//=================================== Task 2 ===========================================
+let button = document.getElementById("alert")
+
+button.addEventListener("click", () => alert("1")); // Запустится этот обработчик и выведет сообщение "1";
+
+button.removeEventListener("click", () => alert("1")); // Игнорируется, потому-что тут другая функция, хотя с таким же именем
+
+button.onclick = () => alert(2); // Запустится этот обработчик и выведет сообщение "2";
+
+// Короче ответ 1 и 2.
+
+//=================================== Task 3 ===========================================
