c278fa93214623636769479e896d709d9ef20a56
Serikbol <sh.serikbol@gmail.com>
2024-05-13 12:31:27 +0500

    added css file for 3rd task

--- index.js
+++ index.js
@@ -7,14 +7,26 @@
 // });
 
 //=================================== Task 2 ===========================================
-let button = document.getElementById("alert")
+//let button = document.getElementById("alert")
 
-button.addEventListener("click", () => alert("1")); // Запустится этот обработчик и выведет сообщение "1";
+//button.addEventListener("click", () => alert("1")); // Запустится этот обработчик и выведет сообщение "1";
 
-button.removeEventListener("click", () => alert("1")); // Игнорируется, потому-что тут другая функция, хотя с таким же именем
+//button.removeEventListener("click", () => alert("1")); // Игнорируется, потому-что тут другая функция, хотя с таким же именем
 
-button.onclick = () => alert(2); // Запустится этот обработчик и выведет сообщение "2";
+//button.onclick = () => alert(2); // Запустится этот обработчик и выведет сообщение "2";
 
 // Короче ответ 1 и 2.
 
 //=================================== Task 3 ===========================================
+const message = document.querySelectorAll('.pane');
+
+message.forEach(function(pane) {
+    pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+    const closeButton = pane.querySelector('.remove-button');
+    closeButton.style.float = 'right';
+  
+    closeButton.addEventListener('click', () => {
+        pane.style.display = 'none';
+    });
+});
+
